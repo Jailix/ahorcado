236db21ed1457cf14f56257145a1bcf25633bb68
JCN21 <terra121@hotmail.es>
2014-11-17 19:02:37 +0000

    2.0v

--- ahorcadovista.php
+++ ahorcadovista.php
@@ -2,66 +2,15 @@
 <head>
     <meta charset="utf-8">
     <title>vista</title>
+    <!--<link rel="icon" href="../../ico.ico">-->
+    <link rel="stylesheet" type="text/css" href="esti.css">
     <?php require_once('funciones.php')?>
     <style>
-        html,body{
-            width: 100%;
-            height: 100%;
-        }
-        body{
-            background-repeat: no-repeat;
-            background: #3b679e; /* Old browsers */
-            background: -moz-linear-gradient(-45deg,  #3b679e 0%, #207cca 27%, #207cca 27%, #2b88d9 56%, #7db9e8 100%); /* FF3.6+ */
-            background: -webkit-gradient(linear, left top, right bottom, color-stop(0%,#3b679e), color-stop(27%,#207cca), color-stop(27%,#207cca), color-stop(56%,#2b88d9), color-stop(100%,#7db9e8)); /* Chrome,Safari4+ */
-            background: -webkit-linear-gradient(-45deg,  #3b679e 0%,#207cca 27%,#207cca 27%,#2b88d9 56%,#7db9e8 100%); /* Chrome10+,Safari5.1+ */
-            background: -o-linear-gradient(-45deg,  #3b679e 0%,#207cca 27%,#207cca 27%,#2b88d9 56%,#7db9e8 100%); /* Opera 11.10+ */
-            background: -ms-linear-gradient(-45deg,  #3b679e 0%,#207cca 27%,#207cca 27%,#2b88d9 56%,#7db9e8 100%); /* IE10+ */
-            background: linear-gradient(135deg,  #3b679e 0%,#207cca 27%,#207cca 27%,#2b88d9 56%,#7db9e8 100%); /* W3C */
-        }
-        .letras{
-            text-align: center;
-            font-size: 300%;
-            float: left;
-            width: 4%;
-            height: 40%;
-            margin: 0 1%;
-            margin-top: 3.5%;
-            background: #feffff; /* Old browsers */
-background: -moz-linear-gradient(-45deg,  #feffff 0%, #ddf1f9 54%, #a0d8ef 100%); /* FF3.6+ */
-background: -webkit-gradient(linear, left top, right bottom, color-stop(0%,#feffff), color-stop(54%,#ddf1f9), color-stop(100%,#a0d8ef)); /* Chrome,Safari4+ */
-background: -webkit-linear-gradient(-45deg,  #feffff 0%,#ddf1f9 54%,#a0d8ef 100%); /* Chrome10+,Safari5.1+ */
-background: -o-linear-gradient(-45deg,  #feffff 0%,#ddf1f9 54%,#a0d8ef 100%); /* Opera 11.10+ */
-background: -ms-linear-gradient(-45deg,  #feffff 0%,#ddf1f9 54%,#a0d8ef 100%); /* IE10+ */
-background: linear-gradient(135deg,  #feffff 0%,#ddf1f9 54%,#a0d8ef 100%); /* W3C */
-filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#feffff', endColorstr='#a0d8ef',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
-        }
+
         #mover{
-            margin-left: 35%;
+                margin-left:<?php echo 50-(strlen($palabra)*2.3)?>%;
         }
-        #separador{
-        background: #a90329; /* Old browsers */
-background: -moz-linear-gradient(-45deg,  #a90329 0%, #1a039e 49%, #140a14 87%); /* FF3.6+ */
-background: -webkit-gradient(linear, left top, right bottom, color-stop(0%,#a90329), color-stop(49%,#1a039e), color-stop(87%,#140a14)); /* Chrome,Safari4+ */
-background: -webkit-linear-gradient(-45deg,  #a90329 0%,#1a039e 49%,#140a14 87%); /* Chrome10+,Safari5.1+ */
-background: -o-linear-gradient(-45deg,  #a90329 0%,#1a039e 49%,#140a14 87%); /* Opera 11.10+ */
-background: -ms-linear-gradient(-45deg,  #a90329 0%,#1a039e 49%,#140a14 87%); /* IE10+ */
-background: linear-gradient(135deg,  #a90329 0%,#1a039e 49%,#140a14 87%); /* W3C */
-filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#a90329', endColorstr='#140a14',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
-            height: 2%;
-        }
-        #letra{
-            height: 20%;
-        }
-        .teclado div{
-            text-align: center;
-            font-size: 200%;
-            float: left;
-            width: 4%;
-            margin: 0 8%;
-        }
-        #mitad{
-            width: 50%;
-        }
+
     </style>
 </head>
 <body>
@@ -73,32 +22,70 @@ filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#a90329', end
         </div>
         <div id="separador">
         </div>
-        <div id="mitad">
-          <div class="teclado">
+        <div id="separar">
+          <div class="teclado mitad">
            <?php
                 echo $letras;
             ?>
           </div>
+          <div>
+           <?php
+                echo $mostrarimagenes;
+            ?>
+          </div>
         </div>
     </div>
+
     <form id="form" action="<?=$_SERVER['PHP_SELF']?>" method="post">
-        <input style="display:hidden" type="text" id="acertada" name="acertadas" value="<?php echo $acertadas?>">
-        <input style="display:hidden" type="text" id="malas" name="malas" value="<?php echo $malas?>">
-        <input style="display:hidden" type="text" id="vidas" name="vidas" value="<?php echo $vidas ?>" required>
-        <input style="display:hidden" type="text" id="letra" name="letra" value="" required>
-        <input style="display:hidden" type="text" id="palabra" name="palabra" value="<?php echo $palabra?>" required>
-        <input style="display:hidden" type="submit">
+        <input type="text" id="acertada" name="acertadas" value="<?php echo $acertadas?>">
+        <input type="text" id="malas" name="malas" value="<?php echo $malas?>">
+        <input type="text" id="vidas" name="vidas" value="<?php echo $vidas ?>" required>
+        <input type="text" id="valor" name="letra" value="">
+        <input type="text" id="palabra" name="palabra" value="<?php echo $palabra?>" required>
+        <input type="submit">
     </form>
+    <button onclick="palabra()">Introduce una palabra nueva</button>
     <script type="text/javascript">
         function mirar(letra,tipo){
             if(tipo==0 || tipo==1){//false
                 alert("Ya fue pulsada");
             }else{
-                document.getElementById("letra").value=letra;
+                document.getElementById("valor").value = letra;
                 document.getElementById("form").submit();
             }
         }
 
+        function palabra (){
+            pala=prompt('Introduce una palabra:','');
+            enviar(pala);
+        }
+        function enviar (pala){
+            document.getElementById("acertada").value="";
+            document.getElementById("malas").value='';
+            document.getElementById("vidas").value=6;
+            document.getElementById("valor").value='a';
+            document.getElementById("palabra").value=pala;
+            document.getElementById("form").submit();
+        }
+        i=<?php echo $numero?>;
+        if(i==0){
+            pala=prompt('Enhorabuena ganastes ponga la nueva palabra:','');
+            if(pala!=null){
+                enviar(pala);
+            }else{
+                perdistes();
+            }
+        }else if(i==2){
+            alert("Perdistes se reseteara la partida con la misma palabra");
+            perdistes();
+        }
+        function perdistes(){
+            document.getElementById("acertada").value="";
+            document.getElementById("malas").value='';
+            document.getElementById("vidas").value=6;
+            document.getElementById("valor").value='a';
+            document.getElementById("form").submit();
+        }
     </script>
 </body>
 </html>
--- funciones.php
+++ funciones.php
@@ -3,6 +3,8 @@ $palabra=isset($_POST['palabra'])? filter_input(INPUT_POST, 'palabra',FILTER_SAN
 if($palabra=="" || ereg("^[a-zA-Z]$", $palabra)){
     header('Location: ahorcado.php');
 }else{
+    $mostrarimagenes='';
+    $numero=1;
     $palabra=strtoupper($palabra);
     $acertadas=isset($_POST['acertadas'])? filter_input(INPUT_POST, 'acertadas',FILTER_SANITIZE_STRING):"H";
 
@@ -17,27 +19,40 @@ if($palabra=="" || ereg("^[a-zA-Z]$", $palabra)){
         $malas.=$letra;
         $vidas=$vidas-1;
     }
-    if($vidas<0){
-        echo "Perdistes";
+    if($vidas<=0){
+        $numero=2;
+    }
+    for($i=5;$i>$vidas;$i--){
+        if($i==5){
+            $mostrarimagenes.="<img id='imagen' alt='foto".$i."' src='pia".$i.".jpg'>";
+        }elseif($i==4){
+            $mostrarimagenes.="<img id='cabe' alt='foto".$i."' src='pia".$i.".jpg'>";
+        }elseif($i==3){
+            $mostrarimagenes.="<img id='cuerpo' alt='foto".$i."' src='pia".$i.".jpg'>";
+        }elseif($i==2){
+            $mostrarimagenes.="<img id='manos' alt='foto".$i."' src='pia".$i.".jpg'>";
+        }elseif($i==1){
+            $mostrarimagenes.="<img id='pies' alt='foto".$i."' src='pia".$i.".jpg'>";
+        }
     }
     $letras=letras($letra,$palabra,$acertadas,$malas);
-    $letrascorrectas=letrasacertadas($palabra,$acertadas);
+    $letrascorrectas=letrasacertadas($palabra,$acertadas,$numero);
 }
 
 //$palabra,$letra
-function letrasacertadas ($palabra,$acertadas){
+function letrasacertadas ($palabra,$acertadas,&$numero){
     $letrascorrectas="";
     $contador=0;
     for($i=0;$i<strlen($palabra);$i++){
         if(strpos($acertadas,$palabra[$i])!==false){
-            if($contador==0){
+            if($i==0){
                  $letrascorrectas.="<div class='letras' id='mover'>".$palabra[$i]."</div>";
             }else{
                 $letrascorrectas.="<div class='letras'>".$palabra[$i]."</div>";
             }
             $contador++;
         }else{
-             if($contador==0){
+             if($i==0){
                  $letrascorrectas.="<div class='letras' id='mover'></div>";
              }else{
                 $letrascorrectas.="<div class='letras'></div>";
@@ -46,11 +61,11 @@ function letrasacertadas ($palabra,$acertadas){
         }
     }
     if($contador==strlen($palabra)){
-        echo "Gano";
+        $numero=0;
     }
     return $letrascorrectas;
 }
-//$letra,$palabra,$acertadas,$malas
+
 function letras($letra,$palabra,$acertadas,$malas){
     $abecedario="ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
     $letras="";
@@ -58,21 +73,21 @@ function letras($letra,$palabra,$acertadas,$malas){
         if($i!=15){
             if(false!==strpos($acertadas,$abecedario[$i])){
                 if($i==14){
-                    $letras.="<div style='background-color:green' onclick=mirar('Ñ',0)>Ñ</div>";
+                    $letras.="<div class='verde' onclick=mirar('ñ',0)>Ñ</div>";
                 }else{
-                    $letras.="<div style='background-color:green' onclick=mirar('".$abecedario[$i]."',0)>".$abecedario[$i]."</div>";
+                    $letras.="<div class='verde' onclick=mirar('".$abecedario[$i]."',0)>".$abecedario[$i]."</div>";
                 }
             }elseif(false!==strpos($malas,$abecedario[$i])){
                 if($i==14){
-                    $letras.="<div style='background-color:red' onclick=mirar('Ñ',1)>Ñ</div>";
+                    $letras.="<div class='rojo' onclick=mirar('ñ',1)>Ñ</div>";
                 }else{
-                    $letras.="<div style='background-color:red' onclick=mirar('".$abecedario[$i]."',1)>".$abecedario[$i]."</div>";
+                    $letras.="<div class='rojo' onclick=mirar('".$abecedario[$i]."',1)>".$abecedario[$i]."</div>";
                 }
             }else{
                 if($i==14){
-                    $letras.="<div style='background-color:blue' onclick=mirar('Ñ',2)>Ñ</div>";
+                    $letras.="<div class='naranja' onclick=mirar('ñ',2)>Ñ</div>";
                 }else{
-                    $letras.="<div style='background-color:blue' onclick=mirar('".$abecedario[$i]."',2)>".$abecedario[$i]."</div>";
+                    $letras.="<div class='naranja' onclick=mirar('".$abecedario[$i]."',2)>".$abecedario[$i]."</div>";
                 }
             }
         }
